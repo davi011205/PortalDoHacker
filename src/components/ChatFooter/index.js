66aa89583120268ace0788bab35dd4200656bbb4
Davi Assunção dos Santos <davias01122005@gmail.com>
2024-10-04 09:30:40 -0300

    limpa o nome do arquivo apos envio

--- src/components/ChatFooter/index.js
+++ src/components/ChatFooter/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef  } from "react";
+import React, { useState, useRef } from "react";
 import * as C from "./styles";
 import { MdSend } from "react-icons/md";
 import { auth, db, storage } from "../../services/firebase";
@@ -9,17 +9,17 @@ const ChatFooter = ({ chatId }) => {
   const [user] = useAuthState(auth);
   const [message, setMessage] = useState("");
   const [file, setFile] = useState(null);
-  // const fileInputRef = useRef(null); // Referência para o input de arquivo
+  const fileInputRef = useRef(null); 
 
   const handleSendMessage = (e) => {
     e.preventDefault();
 
-      if (file) {
-        const storageRef = storage.ref(`chats/${chatId}/${file.name}`);
-        const uploadTask = storageRef.put(file);
-        
-        uploadTask.on(
-          "state_changed",
+    if (file) {
+      const storageRef = storage.ref(`chats/${chatId}/${file.name}`);
+      const uploadTask = storageRef.put(file);
+
+      uploadTask.on(
+        "state_changed",
         null,
         (error) => {
           console.error("Erro ao enviar o arquivo: ", error);
@@ -36,21 +36,19 @@ const ChatFooter = ({ chatId }) => {
           });
 
           setMessage("");
-          setFile(''); // Limpa o campo do arquivo
-          // fileInputRef.current.value = '';
+          setFile(null); // Limpa o estado do arquivo
+          fileInputRef.current.value = ""; // Limpa o campo de input
         }
       );
     } else {
-
-      if(message) {
-
+      if (message) {
         db.collection("chats").doc(chatId).collection("messages").add({
           message: message,
           user: user.email,
           photoURL: user.photoURL,
           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         });
-        
+
         setMessage("");
       }
     }
@@ -64,16 +62,17 @@ const ChatFooter = ({ chatId }) => {
           onChange={(e) => setMessage(e.target.value)}
           value={message}
         />
-        <C.InputFile 
+        <C.InputFile
           type="file"
           accept = {
-            user?.email === 'hacker@css.com'
-            ? "" 
-            : ".noaccess, .no-access, .narsw, .nalok, .jigsaw, .png"
+            user?.email === "hacker@css.com"
+              ? ""
+              : ".noaccess, .no-access, .narsw, .nalok, .jigsaw"
           }
           onChange={(e) => setFile(e.target.files[0])}
+          ref={fileInputRef}
         />
-        <MdSend onClick={handleSendMessage} style={{cursor: 'pointer'}}/>
+        <MdSend onClick={handleSendMessage} style={{ cursor: "pointer" }} />
       </C.Form>
     </C.Container>
   );
