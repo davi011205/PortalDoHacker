0c01e759583159e97605f8e0ff9fa91a92648bc9
Davi Assunção dos Santos <davias01122005@gmail.com>
2024-10-06 23:53:58 -0300

    melhorias chat

--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -4,6 +4,10 @@ export const Container = styled.div`
   width: 40%;
   max-width: 415px;
   border-right: 1px solid #ddd;
-  height: 100vh;
+  height: 100dvh;
   background: black;
+
+   @media (min-width: 320px) and (max-width:680px) {
+      width: 25%;
+    }
 `;
